9cf67da4d46dd4c584af3e06836afe277b6754ae
b7k3 <75824844+b7k3@users.noreply.github.com>
2025-02-13 16:29:46 -0300

    Add files via upload

---
+++ BypassShorteners.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Bypass Multiple Sites
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Bypass multiple ad-link sites automatically
+// @author       Você
+// @match        *://linkvertise.com/*
+// @match        *://lootdest.org/*
+// @match        *://work.ink/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const bypassSites = ["linkvertise.com", "lootdest.org", "work.ink"];
+    const currentUrl = window.location.href;
+
+    if (bypassSites.some(site => currentUrl.includes(site))) {
+        const apiUrl = `https://iwoozie.baby/api/free/bypass?url=${encodeURIComponent(currentUrl)}`;
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                if (data.result) {
+
+                    if (data.result.startsWith("http")) {
+                        window.location.href = data.result;
+                        return
+                    } else {
+                        alert(data.result)
+                    }
+
+                } else {
+                    console.error("Bypass failed: No result in response", data);
+                }
+            })
+            .catch(error => console.error("Error fetching bypass link:", error));
+    }
+})();
